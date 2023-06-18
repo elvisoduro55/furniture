c9ca14923cb53ddb9cdab56bfa6deba529355fe1
elvis-oduro <elvisoduro2000@gmail.com>
2023-06-18 22:37:12 +1300

    done

--- alert.js
+++ alert.js
@@ -5,3 +5,5 @@ document.querySelector(".alert").style.display = "block";
 setTimeout(() => {
   document.querySelector(".alert").style.display = "none";
 }, 3000);
+
+document.getElementById("login").reset();
